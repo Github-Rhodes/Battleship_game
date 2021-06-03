6e7901a42675a84ea67b7fe85b0fae42925eeeb3
Allen Rhodes <war2678@yahoo.com>
2021-06-03 12:14:00 -0500

    grid work

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     const width = 10
 
-    function createBoard(grid, squares, width) {
+    function createBoard(grid, squares) {
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
             square.dataset.id = i
@@ -26,7 +26,67 @@ document.addEventListener('DOMContentLoaded', () =>{
         }
     }
 
-    createBoard(userGrid, userSquares, width)
-    createBoard(computerGrid, computerSquares, width)
+    createBoard(userGrid, userSquares)
+    createBoard(computerGrid, computerSquares)
+
+    const shipArray = [
+        {
+            name: 'destroyer',
+            directions: [
+                [0, 1],
+                [0, width]
+            ]
+        },
+        {
+            name: 'submarine',
+            directions: [
+                [0, 1, 2],
+                [0, width, width*2]
+            ]
+        },
+        {
+            name: 'cruiser',
+            directions: [
+                [0, 1, 2, 3],
+                [0, width, width*2, width*3]
+            ]
+        },
+        {
+            name: 'battleship',
+            directions: [
+                [0, 1, 2, 3, 4],
+                [0, width, width*2, width*3, width*4]
+            ]
+        },
+        {
+            name: 'carrier',
+            directions: [
+                [0, 1, 2, 3, 4, 5],
+                [0, width, width*2, width*3, width*4, width*5]
+            ]
+        },
+    ]
+
+    function generate(ship) {
+        let randomDirection = Math.floor(Math.random() * ship.directions.length)
+        let current = ship.directions[randomDirection]
+        if (randomDirection === 0) direction = 1
+        if (randomDirection === 1) direction = 10
+        let randomStart = Math.floor(Math.random() * computerSquares.length - (ship.directions[0].length * direction))
+
+        const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'))
+        const isAtRightEdge = current.some(index => (randomStart + index) % width === width - 1)
+        const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0)
+
+        if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => computerSquares[randomStart + index].classList.add('taken, ship.name'))
+
+        else generate(ship)
+    }
+
+    generate(shipArray[0])
+    // generate(shipArray[1])
+    // generate(shipArray[2])
+    // generate(shipArray[3])
+    // generate(shipArray[4])
 
 })
